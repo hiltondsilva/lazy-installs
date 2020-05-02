6af9683f1a30d4404f9053984708b4e6a627c0b9
Hilton D'silva <64664466+hiltondsilva@users.noreply.github.com>
2020-05-02 17:43:56 +0500

    Update lazy-installs.sh

--- lazy-installs.sh
+++ lazy-installs.sh
@@ -22,7 +22,21 @@ rm /tmp/ulauncher_5.7.5_all.deb
 sudo apt update -y
 
 echo ------------------------------------------------------------------
-echo ---------- Ulauncher Installed Successfully!! --------------------
+echo ---------- --Ulauncher Installed Successfully!! ------------------
+echo ------------------------------------------------------------------
+
+#Teamviewer
+
+rm /tmp/teamviewer_amd64.deb 
+
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb -O /tmp/teamviewer_amd64.deb
+sudo dpkg -i /tmp/teamviewer_amd64.deb 
+sudo apt --fix-broken install -y
+
+rm /tmp/teamviewer_amd64.deb 
+
+echo ------------------------------------------------------------------
+echo -------------Teamviewer Installed Successfully!! -----------------
 echo ------------------------------------------------------------------
 
 exit
