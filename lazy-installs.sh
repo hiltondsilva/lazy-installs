08c9bfe6befa616d01a7b39758fb7dbfb6cc7a32
Hilton D'silva <64664466+hiltondsilva@users.noreply.github.com>
2020-10-19 17:57:06 +0530

    Update lazy-installs.sh

--- lazy-installs.sh
+++ lazy-installs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
  
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install google-chrome-stable chrome-gnome-shell remmina steam-installer lutris chromium-browser gnome-tweaks deluge vlc flameshot virtualbox
 virtualbox-dkms virtualbox-ext-pack virtualbox-guest-additions-iso refind spotify-client htop neofetch libglib2.0-dev audacity kdenlive go-mtpfs libmtp mtpfs mtp-tools -y
